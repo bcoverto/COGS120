3cf6dd770d37c2c5db6af291b8e9e956fe62cd2e
Brigid <bcoverto@ucsd.edu>
2018-11-15 17:01:34 -0800

    home page interactionscomplete, changed home.html to index.html, and made changes on all pages to reflect that

--- js/project.js
+++ js/project.js
@@ -1,28 +1,34 @@
 
+var newQuestion = {"title":"What's a good substitute for olive oil?", "index":"4", "injectedResult1":"No Results Yet!"}
+
+var saltResponse = {"injectedResult1":"Mint"}
+
+
 
 $(document).ready(function(){
   console.log('ready!');
   myStorage = window.localStorage;
-  //var source = $("#entry-template").html();
-  //var template = Handlebars.compile(source);
 
-  //var parentDiv = $("#templatedProjects");
   $('#postButton').click(post);
+  $('#saltPost').click(postAnswerSalt);
+  $('#beefPost').click(postAnswerBeef)
+  $('#celeryPost').click(postAnswerCelery)
+
 });
 
 function post() {
   var item = document.getElementById("question").value;
-  var description = document.getElementById("description").value;
+  //var description = document.getElementById("description").value;
 
   myStorage = window.localStorage;
   myStorage.setItem('item', item);
-  myStorage.setItem('description', description);
+  //myStorage.setItem('description', description);
 
   item = myStorage.getItem("item");
-  description = myStorage.getItem("description");
+  //description = myStorage.getItem("description");
   var question = "What's a good substitute for " + item + "?";
   console.log(question);
-  console.log(description);
+  //console.log(description);
 
   var x = document.getElementById("snackbar");
 
@@ -32,9 +38,45 @@ function post() {
   // After 3 seconds, remove the show class from DIV
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 
-  $('#MyQuestions').append('<div class="row mt-3"><h5><a href="#" class="mb-3">' + question + '</a></h5> </div>')
-  //parentDiv.append(question);
-  //window.onload = document.getElementById("MyQuestions").innerHTML = myStorage.getItem("item");
+  var source = $("#entry-template1").html();
+  var template = Handlebars.compile(source);
 
-  // '<div class="row mt-3"><h1 class="mb-3">' + question + '</h1> <p>' + description + ' </p> </div>';
+  var parentDiv = $("#templatedQuestion");
+  var html=template(newQuestion);
+  console.log(html);
+  parentDiv.append(html);
+
+}
+
+function postAnswerCelery() {
+
+  var item = document.getElementById("answer2").value;
+  myStorage.setItem('html',"<li class='list-group-item'>" + item + "</li>");
+  console.log(myStorage.getItem('html'))
+  console.log(item)
+  console.log("<li class='list-group-item'>")
+  $('#celeryList').append(myStorage.getItem('html'));
+
+
+}
+function postAnswerBeef() {
+
+  var item = document.getElementById("answer1").value;
+  myStorage.setItem('html',"<li class='list-group-item'>" + item + "</li>");
+  console.log(myStorage.getItem('html'))
+  console.log(item)
+  console.log("<li class='list-group-item'>")
+  $('#beefList').append(myStorage.getItem('html'));
+
+}
+
+function postAnswerSalt() {
+
+  var salt = document.getElementById("answer3").value;
+  myStorage.setItem('html',"<li class='list-group-item'>" + salt + "</li>");
+  console.log(myStorage.getItem('html'))
+  console.log(salt)
+  console.log("<li class='list-group-item'>")
+  $('#saltList').append(myStorage.getItem('html'));
+
 }
